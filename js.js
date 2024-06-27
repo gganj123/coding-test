b0c8fd16ca8dd253225ea6984aa54d3302c1602e
gganj123 <zwol10@naver.com>
2024-06-27 14:49:46 +0900

    fibonacci count 0 1

--- js.js
+++ js.js
@@ -5,10 +5,15 @@ const input = require("fs")
   .split("\n")
   .map((el) => parseInt(el, 10));
 
+let count0 = 0;
+let count1 = 0;
+
 function fibonacci(n) {
   if (n === 0) {
+    count0++;
     return 0;
   } else if (n === 1) {
+    count1++;
     return 1;
   } else {
     return fibonacci(n - 1) + fibonacci(n - 2);
@@ -16,5 +21,11 @@ function fibonacci(n) {
 }
 
 input.forEach((num) => {
-  console.log(fibonacci(num));
+  count0 = 0;
+  count1 = 0;
+  fibonacci(num);
+  //   console.log(`0 count : ${count0}`);
+  //   console.log(`1 count : ${count1}`);
+  console.log(count0, count1);
+  //   console.log(fibonacci(num));
 });
