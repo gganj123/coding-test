2b20b87ee622851ab54e374b20ed74eae4a1a7dd
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-07-16 00:14:27 +0900

    clear

--- 42583.js
+++ 42583.js
@@ -1,20 +1,23 @@
 function solution(bridge_length, weight, truck_weights) {
-  var time = 0;
-  var bridge = [];
-  var bridge_weight = 0;
+  let time = 0;
+  let bridge = [];
+  let bridge_weights = 0;
 
-  for (n = 0; n < bridge_length; n++) {
+  for (let i = 0; i < bridge_length; i++) {
     bridge.push(0);
   }
-  while (truck_weights.length > 0 || truck_weights > 0) {
+
+  while (truck_weights.length > 0 || bridge_weights > 0) {
     time++;
-    var out_truck = bridge.shift();
-    bridge_weight -= out_truck;
+
+    let leaving_truck = bridge.shift();
+    bridge_weights -= leaving_truck;
+
     if (truck_weights.length > 0) {
-      if (bridge_weight + truck_weights[0] <= weight) {
-        var new_truck = truck_weights.shift();
-        bridge.push[new_truck];
-        bridge_weight += new_truck;
+      if (bridge_weights + truck_weights[0] <= weight) {
+        let new_truck = truck_weights.shift();
+        bridge.push(new_truck);
+        bridge_weights += new_truck;
       } else {
         bridge.push(0);
       }
