e682bea8665c02226e6daac142b0f31da546c5b3
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-07-15 23:56:25 +0900

    truck checking

--- 42583.js
+++ 42583.js
@@ -1,4 +1,25 @@
 function solution(bridge_length, weight, truck_weights) {
-    var answer = 0;
-    return answer;
+  var time = 0;
+  var bridge = [];
+  var bridge_weight = 0;
+
+  for (n = 0; n < bridge_length; n++) {
+    bridge.push(0);
+  }
+  while (truck_weights.length > 0 || truck_weights > 0) {
+    time++;
+    var out_truck = bridge.shift();
+    bridge_weight -= out_truck;
+    if (truck_weights.length > 0) {
+      if (bridge_weight + truck_weights[0] <= weight) {
+        var new_truck = truck_weights.shift();
+        bridge.push[new_truck];
+        bridge_weight += new_truck;
+      } else {
+        bridge.push(0);
+      }
+    }
+  }
+
+  return time;
 }
